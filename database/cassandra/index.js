13a26e7724ba65c11b92e575386e1197ae71ecdc
BHamp <brianscotthampton@gmail.com>
2020-05-26 19:58:15 -0400

    Seed cassandra with a script

--- database/cassandra/index.js
+++ database/cassandra/index.js
@@ -1,5 +1,5 @@
-var cassandra = require('cassandra-driver');
-var client = new cassandra.Client({
+const cassandra = require('cassandra-driver');
+const client = new cassandra.Client({
   contactPoints: ['localhost:9042'],
   localDataCenter: 'datacenter1',
 });
@@ -10,34 +10,30 @@ client.connect()
   })
   .then(() => {
     client.execute(
-      'CREATE KEYSPACE IF NOT EXISTS reviewsData WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1}`)';
-    )
+      `CREATE KEYSPACE IF NOT EXISTS reviewData WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1}`
+      ).then(() => {console.log('KEYSPACE CREATED')})
+      .then(() => {
+        client.execute(`DROP TABLE IF EXISTS reviewData.reviews`).then(() => {console.log('TABLE DROPPED')})
+        .then(() => {
+          client.execute(`CREATE TABLE IF NOT EXISTS reviewData.reviews (
+            reviewId INT,
+            page INT,
+            name TEXT,
+            stars INT,
+            country TEXT,
+            date TEXT,
+            review TEXT,
+            image TEXT,
+            title TEXT,
+            avatar INT,
+            foundThisHelpful INT,
+            Primary Key (reviewId)
+          )`).then(() => {
+            console.log('table created')
+         })
+      })
+    })
   })
-  .then(() => {
-    client.execute(`DROP TABLE IF EXISTS reviewData.reviews`)
-  })
-  .then(() => {
-    client.execute(CREATE TABLE IF NOT EXISTS reviewData.reviews (
-      reviewId INT,
-      page INT,
-      name TEXT,
-      stars INT,
-      country TEXT,
-      date TEXT,
-      review TEXT,
-      image TEXT,
-      title TEXT,
-      avatar INT,
-      foundThisHelpful INT
-    ))
-  }.then(() => {
-    console.log('table created')
-  }))
-  .then(() => {
-    client.execute(COPY reviewsData.review (reviewId, page, name, stars, country, date, review, image, title, avatar, foundThisHelpful) FROM 'reviews.csv' WITH DELIMITER = '|' AND HEADER = TRUE);
-  })
-  .catch(() => {
-    console.log('error creating cassandra table')
-  })
-
-  //COPY itemdetails.items (id, productName, producer, answeredQuestions, starPercentages, numberOfRatings, price, inStock, productInfo)
+  .catch((err) => {
+    console.log('error creating cassandra table', err)
+  }); 
